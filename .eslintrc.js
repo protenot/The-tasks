06673248c13c2730e35c3da662f29679a769a325
Olga_Belaya <protenot@yandex.ru>
2023-05-17 15:49:44 +0300

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'jest/globals': true
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
   },
-  'extends': 'google',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
-
+  extends: ["google", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'plugins': ['jest'],
-  'rules': {
-    'import/prefer-default-export': 'off',
-    'comma-dangle': ['error', 'never'],
-    'max-len': [
-      'error', {
-        ignoreComments: true
-      }
-    ]
-  }
+  plugins: ["jest"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "comma-dangle": ["error", "never"],
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+  },
 };
