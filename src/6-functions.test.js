e85328cb9ecf98e4ac3fe35e9406941059a73e12
Olga_Belaya <protenot@yandex.ru>
2023-05-17 18:22:20 +0300

    task-6

---
+++ src/6-functions.test.js
@@ -0,0 +1,22 @@
+import { diff } from "./6-functions";
+describe("diff", () => {
+  it("is a function", () => {
+    expect(diff).toBeInstanceOf(Function);
+  });
+  it("return diff", () => {
+    expect(diff(7, 5)).toEqual(2);
+  });
+});
+
+import { isWord } from "./6-functions.js";
+describe("isWord", () => {
+  it("is a function", () => {
+    expect(isWord).toBeInstanceOf(Function);
+  });
+  it("is string has one item return true", () => {
+    expect(isWord("hello")).toBeTruthy();
+  });
+  it("is string has more then one item return false", () => {
+    expect(isWord("hello my friend")).toBeFalsy();
+  });
+});
