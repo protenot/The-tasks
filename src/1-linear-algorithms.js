466ffd56d3f1b0ae0566f1ab2464cec18677c2e4
Olga_Belaya <protenot@yandex.ru>
2023-05-17 17:46:02 +0300

    task-1

---
+++ src/1-linear-algorithms.js
@@ -0,0 +1,10 @@
+export function multiply(a, b) {
+  console.log(a + b);
+  console.log(a * b);
+  return a + b + " и " + a * b;
+}
+export function summarize(string, superstring) {
+  console.log(string.length + superstring.length);
+  return string.length + superstring.length;
+}
+// summarize ("gnbwf", 'lbhda')
