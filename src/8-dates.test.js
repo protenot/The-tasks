f171f29e4ced1353a931990a9389f7c8bddbee1a
Olga_Belaya <protenot@yandex.ru>
2023-05-23 14:11:53 +0300

    corrected test

--- src/8-dates.test.js
+++ src/8-dates.test.js
@@ -9,11 +9,6 @@ describe("dayOfBirthday", () => {
     expect(dayOfBirthday).toBeInstanceOf(Function);
   });
 
-  it("13.02.1969 will be четверг", () => {
-    jest.spyOn(window, "prompt").mockImplementation(() => "13.02.1969");
-    expect(dayOfBirthday()).toBe("Четверг");
-  });
-
   it("13.02.2023 will be понедельник", () => {
     expect(dayOfBirthday()).toBe("Понедельник");
   });
