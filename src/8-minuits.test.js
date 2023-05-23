dcbf6cbb9e4ae9320a2900da6ac893b0f39be0a3
Olga_Belaya <protenot@yandex.ru>
2023-05-23 14:04:35 +0300

    add timezone in jest.config

--- src/8-minuits.test.js
+++ src/8-minuits.test.js
@@ -6,6 +6,6 @@ describe("getMinuites", () => {
   it("function returns minuites", () => {
     const now = new Date("2023-05-18T15:00:00.000Z");
 
-    expect(getMinuites(now)).toEqual(360);
+    expect(getMinuites(now)).toEqual(540);
   });
 });
